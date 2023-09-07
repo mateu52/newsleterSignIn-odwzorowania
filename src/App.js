b2d8683023b548cd0d2f81337de91162b21db204
mateu52 <wuoelte@gmail.com>
2023-09-07 12:41:11 +0200

    styll app and rwd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './style/style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+      <div className="newsletter">
+        <div className="background-img" >
+
+        </div>
+        <div className="content">
+          <h1>Stay updated</h1>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+          <div className="list">
+            <div className="img" />
+            <p>Product discovery and building what matters</p>
+          </div>
+          <div className="list">
+            <div className="img" />
+            <p>Measuring to ensure updates are a success</p>
+          </div>
+          <div className="list">
+            <div className="img" />
+            <p>And much more!</p>
+          </div>
+          <p className="email">Email address</p>
+          <input placeholder="email@company.com" />
+          <button value="Subscribe to monthly newsletter" />
+        </div>
+       </div> 
     </div>
   );
 }
